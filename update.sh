3d587afe1ff14ab8005df2614cf33435eb7825c8
Shane Whitehead <shanew@swann.com>
2021-06-08 20:34:42 +1000

    Support for xcframeworks

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
 #rm -rf ~/Library/Caches/org.carthage.CarthageKit/dependencies/
 xcodebuild -version
-time carthage12 update --platform iOS --configuration Debug
+#time carthage12 update --platform iOS --configuration Debug
+time carthage update  --use-xcframeworks --platform iOS --configuration Debug --cache-builds $1
