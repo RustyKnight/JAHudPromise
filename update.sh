3f5ba9a6de5c9226ff080f96c9958e362eda5146
Shane Whitehead <shanew@swann.com>
2021-07-13 21:27:21 +1000

    Xcode 13b2

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #rm -rf ~/Library/Caches/org.carthage.CarthageKit/dependencies/
 xcodebuild -version
 #time carthage12 update --platform iOS --configuration Debug
-time carthage update  --use-xcframeworks --platform iOS --configuration Debug --cache-builds $1
+#time carthage update  --use-xcframeworks --platform iOS --configuration Debug --cache-builds $1
+time swncarthage update --skip-simulators --platform iOS --configuration Debug --cache-builds $1
